635559e8298fb259c0627607eec26e32dbbebbca
saode001 <saode001@umn.edu>
2016-04-19 00:45:13 -0500

    U1 working

--- grails-app/assets/javascripts/app/controllers/userDetailController.js
+++ grails-app/assets/javascripts/app/controllers/userDetailController.js
@@ -4,4 +4,20 @@ angular.module('app').controller('userDetailController', function ($resource, $s
 
     $scope.userDetails = AccountDetails.get();
     console.log(AccountDetails);
+
+    var UserMessages = $resource('/api/accounts/:accountId/messages', {accountId: $routeParams.accountId});
+    var Umsg = UserMessages.query();
+    Umsg.$promise.then(function(response){
+      console.log(response);
+        $scope.postings = response});
+   // $scope.userPostings = UserMessages.get();
+
+   // var UserMessages = $resource('/api/accounts/:accountId/messages', {accountId: $routeParams.accountId});
+   // $scope.postings = UserMessages.query();
+   // console.log(postings);
 });
+
+
+
+
+
